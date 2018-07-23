40d78974afd0241a80904a2c6a010b43175a2bf7
marK-Chow <markchow29@gmail.com>
2018-07-23 23:17:21 +0800

    fix speed issue

--- bombcatcher.py
+++ bombcatcher.py
@@ -30,19 +30,21 @@ def print_text(font, x, y, text, color = white):
 
 class Bomb(object):
     def __init__(self):
-        self.x = 300
-        self.y = 250
-        self.speed = 1
+        self.x = 30
+        self.y = 25
+        self.speed = 0.01
         self.radius = 5
         self.color = white
         self.down = True
         self.left = True
 
+
     def move(self):
+
         # Hit the wall
-        if self.x + self.radius + self.speed > screen_w or self.x - self.radius - self.speed < 0 :
+        if self.x + self.radius  > screen_w or self.x - self.radius  < 0 :
             self.left = not self.left
-        if self.y - self.radius - self.speed <= 0 :
+        if self.y - self.radius  <= 0 :
             self.down = not self.down
 
         # Bomb x move
@@ -61,17 +63,10 @@ class Catcher(object):
     def __init__(self):
         self.x = 0
         self.y = 495
-        self.width = 600
+        self.width = 50
         self.height = 5
-        self.speed = 50
+        self.speed = 20
         self.color = blue
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[K_LEFT] and self.x - self.speed >= 0:
-            self.x -= self.speed
-        if keys[K_RIGHT] and (self.x + self.width + self.speed)<= screen_w :
-            self.x += self.speed
 
 class BombCatcher(object):
     def __init__(self):
@@ -81,30 +76,27 @@ class BombCatcher(object):
         self.bomb = Bomb()
         self.catcher = Catcher()
 
-
 bcgame = BombCatcher()
 
 # Game loop
 while True:
-
-    screen.fill((0, 0, 100))
-    pygame.draw.rect(screen, bcgame.catcher.color, (bcgame.catcher.x, bcgame.catcher.y, bcgame.catcher.width, bcgame.catcher.height), 0)
-    pygame.draw.circle(screen, bcgame.bomb.color, (int(bcgame.bomb.x), int(bcgame.bomb.y)), bcgame.bomb.radius, 0)
-
     for ev in pygame.event.get():
         if ev.type == QUIT:
             sys.exit()
-        else :
-            keys = pygame.key.get_pressed()
-            if keys[K_ESCAPE]:
+        elif ev.type == KEYDOWN :
+            if ev.key == K_ESCAPE:
                 sys.exit()
-            else :
-                bcgame.catcher.move()
+            if ev.key == K_LEFT and bcgame.catcher.x - bcgame.catcher.speed >= 0 :
+                bcgame.catcher.x -= bcgame.catcher.speed
+            if ev.key == K_RIGHT and (bcgame.catcher.x + bcgame.catcher.width + bcgame.catcher.speed)<= screen_w :
+                bcgame.catcher.x += bcgame.catcher.speed
+
+    screen.fill((0, 0, 100))
 
     if bcgame.game_over:
-        print_text(font1, 200, 200, "You lost!!!")
+        print_text(font1, 200, 200, "You lost!!! Please <Enter> to restart!!!")
     else :
-        if (bcgame.bomb.y + bcgame.bomb.radius) == bcgame.catcher.y and bcgame.bomb.x >= bcgame.catcher.x and bcgame.bomb.x <= (bcgame.catcher.x + bcgame.catcher.width):
+        if (bcgame.bomb.y + bcgame.bomb.radius) >= bcgame.catcher.y and bcgame.bomb.x >= bcgame.catcher.x and bcgame.bomb.x <= (bcgame.catcher.x + bcgame.catcher.width):
             bcgame.score += 1
             bcgame.bomb.down = False
             bcgame.bomb.move()
@@ -116,5 +108,8 @@ while True:
                 bcgame.catcher.speed = 0
         else :
             bcgame.bomb.move()
+
+    pygame.draw.rect(screen, bcgame.catcher.color, (bcgame.catcher.x, bcgame.catcher.y, bcgame.catcher.width,bcgame.catcher.height), 0)
+    pygame.draw.circle(screen, bcgame.bomb.color, (int(bcgame.bomb.x), int(bcgame.bomb.y)), bcgame.bomb.radius, 0)
 
     pygame.display.update()
