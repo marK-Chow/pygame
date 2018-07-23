dbd5130a483d837287cb405fab61e660798cb001
marK-Chow <markchow29@gmail.com>
2018-07-23 23:44:38 +0800

    A few update

--- bombcatcher.py
+++ bombcatcher.py
@@ -1,6 +1,6 @@
 # version 0.0.0.0.1
 
-import sys, pygame, random
+import sys, pygame, random, time
 
 from pygame.locals import *
 
@@ -30,8 +30,8 @@ def print_text(font, x, y, text, color = white):
 
 class Bomb(object):
     def __init__(self):
-        self.x = 30
-        self.y = 25
+        self.x = random.randint(10, 590)
+        self.y = random.randint(40, 200)
         self.speed = 0.01
         self.radius = 5
         self.color = white
@@ -71,7 +71,7 @@ class Catcher(object):
 class BombCatcher(object):
     def __init__(self):
         self.score = 0
-        self.live = 1
+        self.live = 100
         self.game_over = False
         self.bomb = Bomb()
         self.catcher = Catcher()
@@ -84,12 +84,19 @@ while True:
         if ev.type == QUIT:
             sys.exit()
         elif ev.type == KEYDOWN :
-            if ev.key == K_ESCAPE:
-                sys.exit()
-            if ev.key == K_LEFT and bcgame.catcher.x - bcgame.catcher.speed >= 0 :
-                bcgame.catcher.x -= bcgame.catcher.speed
-            if ev.key == K_RIGHT and (bcgame.catcher.x + bcgame.catcher.width + bcgame.catcher.speed)<= screen_w :
-                bcgame.catcher.x += bcgame.catcher.speed
+            if not bcgame.game_over :
+                if ev.key == K_ESCAPE:
+                    sys.exit()
+                if ev.key == K_LEFT and bcgame.catcher.x - bcgame.catcher.speed >= 0 :
+                    bcgame.catcher.x -= bcgame.catcher.speed
+                if ev.key == K_RIGHT and (bcgame.catcher.x + bcgame.catcher.width + bcgame.catcher.speed)<= screen_w :
+                    bcgame.catcher.x += bcgame.catcher.speed
+                if ev.key == K_UP:
+                    bcgame.bomb.speed += 0.01
+                if ev.key == K_DOWN and bcgame.bomb.speed > 0.01 :
+                    bcgame.bomb.speed -= 0.01
+            elif ev.key == K_KP_ENTER or ev.key == K_RETURN :
+                bcgame = BombCatcher()
 
     screen.fill((0, 0, 100))
 
@@ -99,17 +106,23 @@ while True:
         if (bcgame.bomb.y + bcgame.bomb.radius) >= bcgame.catcher.y and bcgame.bomb.x >= bcgame.catcher.x and bcgame.bomb.x <= (bcgame.catcher.x + bcgame.catcher.width):
             bcgame.score += 1
             bcgame.bomb.down = False
+            if bcgame.score % 5 == 0:
+                bcgame.bomb.speed += 0.01
             bcgame.bomb.move()
-        elif (bcgame.bomb.y + bcgame.bomb.radius) == screen_h:
+        elif (bcgame.bomb.y + bcgame.bomb.radius) >= screen_h:
             bcgame.live -= 1
+
             if bcgame.live == 0 :
                 bcgame.game_over = True
-                bcgame.bomb.speed = 0
-                bcgame.catcher.speed = 0
+            else :
+                time.sleep(0.3)
+                bcgame.bomb = Bomb()
         else :
             bcgame.bomb.move()
 
     pygame.draw.rect(screen, bcgame.catcher.color, (bcgame.catcher.x, bcgame.catcher.y, bcgame.catcher.width,bcgame.catcher.height), 0)
     pygame.draw.circle(screen, bcgame.bomb.color, (int(bcgame.bomb.x), int(bcgame.bomb.y)), bcgame.bomb.radius, 0)
-
+    print_text(font1, 5, 5, "Score : " + str(bcgame.score))
+    print_text(font1, 5, 35, "Live : " + str(bcgame.live))
+    print_text(font1, 500, 5, "Speed : " + str(round(bcgame.bomb.speed,2)))
     pygame.display.update()
